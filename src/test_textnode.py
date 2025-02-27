cad1f7938efab1fa79a28f86dca6b6571eb473de
tnarkiv <vikrant2099ad@gmail.com>
2025-02-27 03:56:58 +0000

    Added test cases

---
+++ src/test_textnode.py
@@ -0,0 +1,44 @@
+"""This file contains test cases for text nodes"""
+
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    """Class TestTextNode
+
+    Args:
+        unittest (unittest.TestCase): base class TestCase
+    """
+
+    def test_eq(self):
+        """This test creates two TextNode objects with the same properties 
+        and asserts that they are equal."""
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_text_not_eq(self):
+        """This test creates two TextNode objects with different texts and
+        asserts that they are not equal."""
+        node = TextNode("Random text", TextType.ITALICS)
+        node2 = TextNode("Not Random text", TextType.ITALICS)
+        self.assertNotEqual(node, node2)
+
+    def test_text_type_not_eq(self):
+        """This test creates two TextNode objects with different text types and
+        asserts that they are not equal."""
+        node = TextNode("Random text", TextType.BOLD)
+        node2 = TextNode("Not Random text", TextType.ITALICS)
+        self.assertNotEqual(node, node2)
+
+    def test_url_not_eq(self):
+        """This test creates two TextNode objects with different texts and
+        asserts that they are not equal."""
+        node = TextNode("Random text", TextType.ITALICS, "https://www.google.com")
+        node2 = TextNode("Not Random text", TextType.ITALICS, "https://www.yahoo.co.in")
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -21,7 +21,7 @@ class TextType(Enum):
 class TextNode:
     """Text node class"""
 
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
